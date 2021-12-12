224bae098e37f06d068097c22a1475218d68881c
Anas <anastasiapome@gmail.com>
2021-12-12 02:38:38 +0100

    toggle setter

---
+++ Assets/Scripts/UI/ToggleSetter.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ToggleSetter : MonoBehaviour
+    {
+        [Header("Internal References")]
+        [SerializeField] private Toggle editToggle;
+        [SerializeField] private Toggle deleteToggle;
+        [SerializeField] private Toggle trajectoryToggle;
+
+        public void SetToggleCorrectly(bool edit, bool delete)
+        {
+            if (edit && delete)
+            {
+                editToggle.SetIsOnWithoutNotify(false);
+                deleteToggle.SetIsOnWithoutNotify(true);
+                trajectoryToggle.SetIsOnWithoutNotify(false);
+            }
+            else if(edit)
+            {
+                editToggle.SetIsOnWithoutNotify(true);
+                deleteToggle.SetIsOnWithoutNotify(false);
+                trajectoryToggle.SetIsOnWithoutNotify(false);
+            }
+            else if(!delete)
+            {
+                editToggle.SetIsOnWithoutNotify(false);
+                deleteToggle.SetIsOnWithoutNotify(false);
+                trajectoryToggle.SetIsOnWithoutNotify(true);
+            }
+        }
+        
+    }
+}
