edd5e50e7fb1e96b81562a537fa80b5caf9540df
Markus Klaus <Markus.klaus@tum.de>
2021-12-12 11:37:52 +0100

    ton

---
+++ Assets/Scripts/BGMScript.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMScript : MonoBehaviour
+{
+    private static BGMScript instance = null;
+
+    public static BGMScript Instance
+    {
+        get { return instance; }
+    }
+
+    [SerializeField] private AudioClip buttonPressSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            AudioSource.PlayClipAtPoint(buttonPressSound,new Vector3(0,0,-10));
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    
+}
